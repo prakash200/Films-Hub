c661b2eb6980b5c4bb5c99cabc342ececc5088bc
prakash <prakashnani130@gmail.com>
2023-01-08 19:49:56 +0530

    modified navbar using react hook function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/navbar";
+import { Container } from "@mui/system";
+import Trending from "./Pages/Trending/trending";
+import Series from "./Pages/Series/series";
+import Movies from "./Pages/Movies/movies";
+import Search from "./Pages/Search/search";
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="app">ffff</div>
-      <SimpleBottomNavigation />
+      <BrowserRouter>
+        <Header />
+        <div className="app">
+          <Container>
+            <Routes>
+              <Route path="/" element={<Trending />} exact />
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/series" element={<Series />} />
+              <Route path="/search" element={<Search />} />
+            </Routes>
+          </Container>
+        </div>
+        <SimpleBottomNavigation />
+      </BrowserRouter>
     </>
   );
 }
